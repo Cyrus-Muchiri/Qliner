9df63dc6580971dd6ed26705a2e67f5e892f6fd9
C_Muchiri <cyrusmuchirichomba@gmail.com>
2019-07-23 20:05:05 +0300

    Created a post request to API, working. However, service_id is hardcoded

--- src/ticketing/mobileNumber.js
+++ src/ticketing/mobileNumber.js
@@ -6,20 +6,37 @@ function submitMobile() {
     const mobile = document.getElementById('mobilePhone').value;
 
     addCustomerRequest(mobile);
+
 }
 
-function addCustomerRequest(mobile){
+function addCustomerRequest(mobile) {
+    var response;
     var request = new XMLHttpRequest();
-    request.open("POST", 'http://localhost/qliner_api/ticketing/addCustomer', true);
+    request.open("POST", 'http://localhost/qliner_api/ticketing/addCustomer');
     request.setRequestHeader('Content-Type', 'application/json');
-    let data = JSON.stringify({
+    let data =JSON.stringify({
         'mobile': mobile,
-        'service_id' : 3
+        'service_id': 3
     });
-    console.log(data);
     request.send(data);
 
     request.onreadystatechange = (e) => {
-        console.log(request.responseText)
-    }
+       response = JSON.parse(request.responseText);
+      // response = request.responseText;
+        var response_div = document.getElementById('response');
+        response_div.style.display = 'block';
+        console.log(response);
+
+        if(response.status == true) {
+            console.log(response);
+            response_div.innerHTML = response['result'];
+            response_div.classList.add('alert-success');
+
+        }else{
+            console.log('false');
+            response_div.innerHTML = response['result'];
+            response_div.classList.add('alert-success');
+        }
+    };
+
 }
