750c6a1ebef8bb13fcfadfa78fd75a7980693957
StanMandela <38228021+StanMandela@users.noreply.github.com>
2019-08-01 21:57:48 +0300

    bashm

---
+++ src/ticketing/mobileNumber.js
@@ -0,0 +1,47 @@
+const {electron,remote} = require('electron');
+const ipc = electron.ipcRenderer;
+function submitMobile() {
+    const form = document.getElementById('mobileForm');
+    form.onsubmit = (function (e) {
+        e.preventDefault();
+    });
+    const mobile = document.getElementById('mobilePhone').value;
+    //ipc.send('service_id',service_id);
+
+
+    addCustomerRequest(mobile);
+
+}
+
+function addCustomerRequest(mobile) {
+    var response;
+    var request = new XMLHttpRequest();
+    request.open("POST", 'http://localhost/qliner_api/ticketing/addCustomer');
+    request.setRequestHeader('Content-Type', 'application/json');
+    var service = remote.getGlobal('service_id') ;
+    let data =JSON.stringify({
+        'mobile': mobile,
+        'service_id': service
+    });
+    request.send(data);
+
+    request.onreadystatechange = (e) => {
+    //   response = JSON.parse(request.responseText);
+      response = request.responseText;
+      console.log(response);
+        var response_div = document.getElementById('response');
+        response_div.style.display = 'block';
+
+        if(response.status == true) {
+
+            response_div.innerHTML = response['result'];
+            response_div.classList.add('alert-success');
+
+        }else{
+           // console.log('false');
+            response_div.innerHTML = response['result'];
+            response_div.classList.add('alert-success');
+        }
+    };
+
+}
---
+++ src/welcome/welcome.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const path = require('path');
+const BrowserWindow = electron.remote.BrowserWindow;
+
+const ticketingButton = document.getElementById('ticketing');
+ticketingButton.addEventListener('click', function (event) {
+    const modalPath = path.join('file://', __dirname, '../ticketing/ticketing.html');
+    console.log(modalPath);
+    let win = new BrowserWindow({
+        width: 1200,
+        height: 700,
+        webPreferences:{
+            nodeIntegration:true,
+            webSecurity:false
+
+        }
+    })
+    win.on('close', function () { win = null });
+    win.loadURL(modalPath);
+    win.show()
+});
+const insightsButton = document.getElementById('insights');
+insightsButton.addEventListener('click', function (event) {
+    const modalPath = path.join('file://', __dirname, '../insights/insights.html');
+    let win = new BrowserWindow({
+
+    })
+    win.on('close', function () { win = null })
+    win.loadURL(modalPath)
+    win.show()
+});
+const displayButton = document.getElementById('display')
+displayButton.addEventListener('click', function (event) {
+    const modalPath = path.join('file://', __dirname, '../queueingDisplay/informativeDisplay.html')
+    let win = new BrowserWindow({
+    
+         width: 1400,
+         height: 900,
+        
+    });
+    win.on('close', function () { win = null })
+    win.loadURL(modalPath)
+    win.show()
+});
