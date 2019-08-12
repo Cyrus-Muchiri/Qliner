56274de3a8b719955dd662884b0d4dea31a489e4
C_Muchiri <cyrusmuchirichomba@gmail.com>
2019-08-12 23:27:12 +0300

    Server show five or less records in queue, next customer action starts

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,10 @@
 const {electron,remote} = require('electron');
-
+    var now_serving;
+    var next_customer;
+    var service_id
  window.onload = function () {
     var service_details = remote.getGlobal('service_details');
-    var service_id = service_details['service_id'];
+    service_id = service_details['service_id'];
     var service_name = service_details['service'];
     document.getElementById('service_title').innerText = service_name;
     var response;
@@ -14,67 +16,77 @@ const {electron,remote} = require('electron');
     });
     request.send(data);
 
+
     request.onreadystatechange = (e) => {
-           response = JSON.parse(request.responseText);
-        //response = request.responseText;
+          response = JSON.parse(request.responseText);
+           console.log(response);
+       // response = request.responseText;
+        //console.log(response);
+
         var table_data = '';
         var tbody = document.getElementById('customers_in_queue');
+        var now_serving_dom = document.getElementById('now_serving');
+        var next_customer_dom = document.getElementById('next_customer');
         if (response.status == true) {
         var customers = response.customers;
             for (var i = 0; i < customers.length; i++) {
-                 table_data += "<tr><td>"+(i+1)+"</td><td>" + response.customers[i].ticket_no + "</td></tr>";
+                if(i == 0){
+                    now_serving = customers[i].ticket_no;
+                }else if(i==1){
+                    next_customer = customers[i].ticket_no;
+                }
+                 table_data += "<tr><td>"+(i+1)+"</td><td>" + customers[i].ticket_no + "</td></tr>";
             }
         } else {
                 table_data = "<td>No customers</td>";
         }
         tbody.innerHTML =table_data;
+        now_serving_dom.innerText = now_serving;
+        next_customer_dom.innerText = next_customer;
 
     };
 }
 
 function nextCustomer() {
-    const ipc = require('electron').ipcRenderer
-    ipc.send('ser',"Hey rus");
-
-    //1 : display 2: server
-   // ipc.sendTo(1,'next',"Men");
-    //refresh table
-    //refresh display
-    //mark prev customer served
-    //call next customer
-    //send sms to the next customer
-
-}
-
-function addCustomerRequest(mobile) {
     var response;
     var request = new XMLHttpRequest();
-    request.open("POST", 'http://localhost/qliner_api/ticketing/addCustomer');
+    request.open("POST", 'http://localhost/qliner_api/service/nextCustomer');
     request.setRequestHeader('Content-Type', 'application/json');
-    var service = remote.getGlobal('service_id');
     let data = JSON.stringify({
-        'mobile': mobile,
-        'service_id': service
+        "current" : now_serving,
+        "next" : next_customer,
+        "service_id":service_id
     });
     request.send(data);
 
     request.onreadystatechange = (e) => {
-        //   response = JSON.parse(request.responseText);
-        response = request.responseText;
-        console.log(response);
-        var response_div = document.getElementById('response');
-        response_div.style.display = 'block';
+        response = JSON.parse(request.responseText);
 
-        if (response.status == true) {
-
-            response_div.innerHTML = response['result'];
-            response_div.classList.add('alert-success');
-
-        } else {
-            // console.log('false');
-            response_div.innerHTML = response['result'];
-            response_div.classList.add('alert-success');
+        if(response.status == true){
+            var table_data = '';
+            var tbody = document.getElementById('customers_in_queue');
+            var now_serving_dom = document.getElementById('now_serving');
+            var next_customer_dom = document.getElementById('next_customer');
+            if (response.status == true) {
+            var customers = response.customers;
+                for (var i = 0; i < customers.length; i++) {
+                    if(i == 0){
+                        now_serving = customers[i].ticket_no;
+                    }else if(i==1){
+                        next_customer = customers[i].ticket_no;
+                    }
+                     table_data += "<tr><td>"+(i+1)+"</td><td>" + customers[i].ticket_no + "</td></tr>";
+                }
+            } else {
+                    table_data = "<td>No customers</td>";
+            }
+            tbody.innerHTML =table_data;
+            now_serving_dom.innerText = now_serving;
+            next_customer_dom.innerText = next_customer;
         }
-    };
+    }
+
+    
 
 }
+
