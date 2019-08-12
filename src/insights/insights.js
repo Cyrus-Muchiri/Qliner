fb0bf03c822f09c4eb51a08dc4963b503665afd3
StanMandela <38228021+StanMandela@users.noreply.github.com>
2019-08-12 17:49:00 +0300

    login UI done with

--- src/insights/insights.js
+++ src/insights/insights.js
@@ -1,6 +1,7 @@
 var server_utilizations;
+var queue_length;
 window.onload = function () {
-    var start_Date = "2019-07-31";
+    var start_Date = "2019-07-23";
     var end_Date = "2019-07-31";
 
     var response;
@@ -17,16 +18,19 @@ window.onload = function () {
         response = JSON.parse(request.responseText);
        //response = request.responseText;
         var server_utilizations;
+        var customers_in_queue;
        if (response.status  == "true"){
           server_utilizations = response.server_utilization;
+          customers_in_queue = response.customers;
        }
         var categories = [];
         var utils= [];
 
+
             for (var i = 0; i < server_utilizations.length; i++) {
                 utils.push([server_utilizations[i].utilizations]);
             }
-            var ut = utils.join()
+
         for (var i = 0; i < server_utilizations.length; i++) {
             categories.push([server_utilizations[i].date]);
         }
@@ -68,6 +72,60 @@ window.onload = function () {
             series: [{
                 name: 'Server Utils',
                 data: utils
+            }]
+        });
+
+        // Graph for queue lengths on selected dates
+        //geting queue length
+        var queue= [];
+        var days = [];
+
+
+        for (var i = 0; i < customers_in_queue.length; i++) {
+            queue.push([customers_in_queue[i].queue_length]);
+        }
+
+        for (var i = 0; i < customers_in_queue.length; i++) {
+            days.push([customers_in_queue[i].date]);
+        }
+        Highcharts.chart('Queue_lengths', {
+            chart: {
+                type: 'column',
+                options3d: {
+                    enabled: true,
+                    alpha: 10,
+                    beta: 25,
+                    depth: 70
+                }
+            },
+            title: {
+                text: 'queue lengths'
+            },
+            subtitle: {
+                text: 'Depicts queue lengths by date'
+            },
+            plotOptions: {
+                column: {
+                    depth: 25
+                }
+            },
+            xAxis: {
+                categories: days,
+                labels: {
+                    skew3d: true,
+                    style: {
+                        fontSize: '16px'
+                    }
+                }
+            },
+            yAxis: {
+                title: {
+                    text: null
+                }
+            },
+            series: [{
+                name: 'Customers in queue',
+                data: queue
             }]
         });
 
